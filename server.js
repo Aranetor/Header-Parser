afb59cb2790edf0da70adb37eea8157ca4d2105f
Aranetor <p.aranetor@gmail.com>
2017-01-07 01:25:31 +0100

    working !

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res){
-	var fileName = path.join(__dirname, 'index.html');
-	res.sendFile(fileName, function (err) {
-		if (err) {
-			console.log(err);
-			res.status(err.status).end();
-		}
-		else {
-			console.log('Sent:', fileName);
-		}
-	});
+app.get("/whoiam", function(req, res){
+	var ip=req.connection.remoteAddress.toString().split(":")[3];
+	var lang=req.get("Accept-Language").split(",")[0];
+	var os=req.get("User-Agent").split("(")[1].split(")")[0];
+	
+	res.end(JSON.stringify({ipadress:ip,language:lang,software:os}));
 });
 
 app.listen(process.env.PORT || 3000, function(){
-	console.log("Timestamp microservice running !");
+	console.log("Header parser running !");
 });
